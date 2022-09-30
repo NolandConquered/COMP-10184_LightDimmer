e5a70a58bb60312f863c37a4c36ab59ead0a8556
NolandConquered <62732585+NolandConquered@users.noreply.github.com>
2022-09-30 11:32:45 -0400

    Add files via upload

---
+++ PulseWidthModulation/src/main.cpp
@@ -0,0 +1,29 @@
+// Mohawk College - COMP-10184 
+// 
+// Pulse Width Modulation - Test Program 
+// 
+#include <Arduino.h> 
+ 
+void setup() { 
+  // configure the USB serial monitor 
+  Serial.begin(115200); 
+ 
+  // configure pin D4 as a digital output - this is the LED 
+  pinMode(D4, OUTPUT); 
+ 
+  // set default PWM range 
+  analogWriteRange(1023); 
+} 
+ 
+ 
+void loop() { 
+  int i; 
+   
+  for ( i=0; i<1024; ++i) { 
+    // set the PWM pulse width 
+    analogWrite(D4, i); 
+ 
+    // wait 1ms to add DRAMA! 
+    delay(1); 
+  } 
+} 
